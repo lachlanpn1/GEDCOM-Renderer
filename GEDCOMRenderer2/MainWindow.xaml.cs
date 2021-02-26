0a745280b5706d868416b0921e948a10c8a39f59
Lachlan <lachlan.p.n@outlook.com>
2021-02-26 21:19:32 +1100

    first commit

---
+++ GEDCOMRenderer2/MainWindow.xaml.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Diagnostics;
+using System.Windows.Shapes;
+using GEDCOMLibrary;
+
+namespace GEDCOMRenderer2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const double ScaleRate = 1.1;
+        bool IsMouseDown;
+        double xOffset;
+        double yOffset;
+        Point initial;
+        Point endPoint = new Point(0,0);
+
+        List<TreeMember> family = new List<TreeMember>();
+        FamilyTree testFamily = new FamilyTree();
+        Point firstMemberPoint = new Point(960, 0);
+        double initialWidth = Convert.ToDouble(ConfigurationManager.AppSettings.Get("Generation0LineWidth"));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerateTreeMembers();
+
+            Loaded += delegate
+            {
+                PositionTree(family[0]);
+            };
+
+        }
+
+        private FamilyTree InitializeTestFamily()
+        {
+            FamilyTree familyTree = new FamilyTree();
+            Person p = new Person();
+            p.GivenName = "Jim";
+            p.FamilyName = "Jones";
+            p.Gender = Gender.Male;
+            p.Events.Add(new GEDCOMLibrary.Event()
+            {
+                EventType = EventType.Birth,
+                Date = new DateTime(1900, 1, 12),
+                Location = "Testville, USA",
+            });
+            p.Events.Add(new GEDCOMLibrary.Event()
+            {
+                EventType = EventType.Death,
+                Date = new DateTime(1981, 3, 23),
+                Location = "New York, USA",
+            });
+            Person p2 = new Person();
+            p2.GivenName = "Samantha";
+            p2.FamilyName = "Jones";
+            p2.MaidenName = "Wilkins";
+            p2.Gender = Gender.Female;
+            p2.Events.Add(new GEDCOMLibrary.Event()
+            {
+                EventType = EventType.Birth,
+                Date = new DateTime(1902, 5, 7),
+                Location = "Testville, USA",
+            });
+            p2.Events.Add(new GEDCOMLibrary.Event()
+            {
+                EventType = EventType.Death,
+                Date = new DateTime(1983, 5, 21),
+                Location = "New York, USA",
+            });
+            Person p3 = new Person();
+            p3.GivenName = "Willie";
+            p3.FamilyName = "Jones";
+            p3.Gender = Gender.Male;
+            p3.Events.Add(new GEDCOMLibrary.Event()
+            {
+                EventType = EventType.Birth,
+                Date = new DateTime(1935, 12, 2),
+                Location = "Testville, USA",
+            });
+            p3.Events.Add(new GEDCOMLibrary.Event()
+            {
+                EventType = EventType.Death,
+                Date = new DateTime(2018, 7, 15),
+                Location = "New York, USA",
+            });
+            Family family = new Family()
+            {
+                Husband = p,
+                Wife = p2,
+            };
+            family.AddChild(p3);
+            p3.ChildFamily = family;
+            p.SpouseFamily = family;
+            p2.SpouseFamily = family;
+            familyTree.Members.Add(p3);
+            familyTree.Members.Add(p);
+            familyTree.Members.Add(p2);
+            return familyTree;
+        }
+
+        private void ZoomInButton_Click(object sender, RoutedEventArgs e)
+        {
+            st.ScaleX *= ScaleRate;
+            st.ScaleY *= ScaleRate;
+        }
+
+        private void ZoomOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            st.ScaleX /= ScaleRate;
+            st.ScaleY /= ScaleRate;
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            IsMouseDown = true;
+            initial = Mouse.GetPosition(this);
+            xOffset = 0.0;
+            yOffset = 0.0;
+            Debug.Write("Mouse left button down \n");
+            Debug.WriteLine("Initial position : " + initial);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsMouseDown) return;
+            xOffset = e.GetPosition(this).X - initial.X;
+            Debug.WriteLine("xOffset: " + xOffset);
+            yOffset = e.GetPosition(this).Y - initial.Y;
+            Debug.WriteLine("yOffset: " + yOffset);
+            tt.X = endPoint.X + xOffset;
+            Debug.WriteLine("tt.X : " + tt.X);
+            tt.Y = endPoint.Y + yOffset;
+            Debug.WriteLine("yy.Y : " + tt.Y);
+
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            IsMouseDown = false;
+            endPoint.X += xOffset;
+            endPoint.Y += yOffset;
+            Debug.Write("Mouse left button up\n");
+            Debug.WriteLine("ENDPOINT.X = " + endPoint.X);
+            Debug.WriteLine("ENDPOINT.Y = " + endPoint.Y);
+        }
+
+        private void Canvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            IsMouseDown = false;
+            Debug.Write("Mouse leave\n");
+            
+        }
+
+        private void Canvas_DragLeave(object sender, DragEventArgs e)
+        {
+            IsMouseDown = false;
+            Debug.WriteLine("Drag leave\n");
+        }
+
+        public void SetTreeMemberLocation(TreeMember treeMember, Point location)
+        {
+            Canvas.SetLeft(treeMember, location.X);
+            Canvas.SetTop(treeMember, location.Y);
+            treeMember.Location = location;
+            treeMember.HasBeenPositioned = true;
+        }
+
+        // Recursive Function that positions the Family Tree
+        private void PositionTreeMember(TreeMember member, Point location)
+        {
+            Person person = member.Person;
+            // position self
+            SetTreeMemberLocation(member, location);
+            if(person.HasParents())
+            {
+                // draw parent line
+                canvas.Children.Add(member.GetParentLine());
+                if(person.HasFather())
+                {
+                    TreeMember father = FindPersonAsTreeMember(person.ChildFamily.Husband);
+                    Point fatherLocation = member.GetParentLocation(father.Person.HasSpouse(), father);
+                    PositionTreeMember(father, fatherLocation);
+                }
+                if(person.HasMother())
+                {
+                    TreeMember mother = FindPersonAsTreeMember(person.ChildFamily.Wife);
+                    Point motherLocation = member.GetParentLocation(mother.Person.HasSpouse(), mother);
+                    PositionTreeMember(mother, motherLocation);
+                }
+            }
+            if(person.Gender == Gender.Male && person.HasSpouse())
+            {
+                // if male & has spouse draw spouse line
+                canvas.Children.Add(member.GetSpouseLine());
+            }
+            // call method on children if they've still not be drawn
+            if(person.HasChildren())
+            {
+                // not implemented.
+            }
+        }
+
+        private void GenerateTreeMembers()
+        {
+            String file = "noonan.ged";
+            GEDCOMReader gcr = new GEDCOMReader();
+            StreamReader sr = new StreamReader(file);
+            //FamilyTree ft = gcr.ProcessFile(sr);
+            FamilyTree ft = InitializeTestFamily();
+
+            family = GenerateTreeMember(ft.Members[0], initialWidth);
+
+            foreach (TreeMember tm in family)
+            {
+                canvas.Children.Add(tm);
+            }
+
+        }
+
+        private List<TreeMember> GenerateTreeMember(Person p, double lineWidth)
+        {
+            TreeMember tm = new TreeMember(p, lineWidth);
+            List<TreeMember> result = new List<TreeMember>();
+            result.Add(tm);
+
+            if(p.HasParents())
+            {
+                if(p.HasFather())
+                {
+                    Person father = p.ChildFamily.Husband;
+                    List<TreeMember> fatherTree = GenerateTreeMember(father, (0.400 * lineWidth));
+                    result.AddRange(fatherTree);
+                }
+                if(p.HasMother())
+                {
+                    Person mother = p.ChildFamily.Wife;
+                    List<TreeMember> motherTree = GenerateTreeMember(mother, (0.400 * lineWidth));
+                    result.AddRange(motherTree);
+                }
+            }
+            return result;
+        }
+
+        private void PositionTree(TreeMember firstMember)
+        {
+            //Queue<TreeMember> membersToPlace = new Queue<TreeMember>();
+            //membersToPlace.Enqueue(firstMember);
+            PositionTreeMember(firstMember, firstMemberPoint);
+
+        }
+
+        private TreeMember FindPersonAsTreeMember(Person person)
+        {
+            return (family.Find(member => member.Person == person));
+        }
+    }
+}
+
---
+++ GEDCOMRenderer2/TreeMember.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GEDCOMLibrary;
+
+namespace GEDCOMRenderer2
+{
+    /// <summary>
+    /// Interaction logic for TreeMember.xaml
+    /// </summary>
+    public partial class TreeMember : UserControl
+    {
+        double CHILD_LINE_LENGTH = Convert.ToDouble(ConfigurationManager.AppSettings.Get("ChildLineLength"));
+
+        public Point Location { get; set; }
+        public double SpouseLineWidth { get; set; }
+        public double ChildLineXOffset { get; set; }
+        public double ParentLineXOffset { get; set; }
+        public double ChildLineWidth { get; set; }
+        public double ParentLineWidth { get; set; }
+        public bool HasBeenPositioned { get; set; } = false;
+
+        public Person Person { get; set; }
+
+        public TreeMember(Person person)
+        {
+            InitializeComponent();
+            this.Person = person;
+            TreeMemberName.Text = this.Person.GetDisplayName(true);
+            TreeMemberDate.Text = this.Person.GetLifeDatesAsString();
+        }
+
+        public TreeMember(Person person, double lineWidth)
+        {
+            InitializeComponent();
+            double generation = GetGeneration(lineWidth);
+            this.Person = person;
+            TreeMemberName.Text = this.Person.GetDisplayName(true);
+            TreeMemberDate.Text = this.Person.GetLifeDatesAsString();
+
+
+            double TreeMemberNameFontSize = CalculateTreeMemberNameFontSize(generation);
+            double TreeMemberDateFontSize = CalculateTreeMemberDateFontSize(generation);
+
+            TreeMemberName.FontSize = TreeMemberNameFontSize;
+            TreeMemberDate.FontSize = TreeMemberDateFontSize;
+
+            SpouseLineWidth = lineWidth;
+            ChildLineXOffset = 0.5 * lineWidth;
+            ParentLineXOffset = ChildLineXOffset;
+            ChildLineWidth = CHILD_LINE_LENGTH;
+            ParentLineWidth = ChildLineWidth;
+        }
+
+        public Point GetRightAnchor()
+        {
+            double anchorX = Location.X + this.ActualWidth;
+            double anchorY = Location.Y + (this.ActualHeight / 2);
+            return new Point(anchorX, anchorY);
+        }
+
+        public Point GetLeftAnchor()
+        {
+            double anchorX = Location.X;
+            double anchorY = Location.Y + (this.ActualHeight / 2);
+            return new Point(anchorX, anchorY);
+        }
+
+        public Point GetTopAnchor()
+        {
+            double anchorX = Location.X + (this.ActualWidth / 2);
+            double anchorY = Location.Y;
+            return new Point(anchorX, anchorY);
+        }
+
+        public Point GetBottomAnchor()
+        {
+            double anchorX = Location.X + (this.ActualWidth * 0.5);
+            double anchorY = Location.Y + this.ActualHeight;
+            return new Point(anchorX, anchorY);
+        }
+
+        public Line GetChildLine(bool hasSpouse)
+        {
+            Line line = new Line();
+            line.StrokeThickness = Convert.ToDouble(ConfigurationManager.AppSettings.Get("ChildLineThickness"));
+            line.Stroke = (Brush)Application.Current.Resources["ChildLineBrush"];
+            // MaleLocation.
+            if (Person.Gender == Gender.Male)
+            {
+                line.X1 = hasSpouse ? GetRightAnchor().X + ChildLineXOffset : Location.X + (0.5 * this.ActualWidth);
+                line.Y1 = hasSpouse ? Location.Y + (0.5 * this.ActualHeight) : Location.Y + this.ActualHeight;
+                line.X2 = line.X1;
+                line.Y2 = Location.Y + ChildLineWidth;
+            }
+            // Female
+            if (Person.Gender == Gender.Female)
+            {
+                line.X1 = hasSpouse ? GetRightAnchor().X - ChildLineXOffset : Location.X + (0.5 * this.ActualWidth);
+                line.Y1 = hasSpouse ? Location.Y + (0.5 * this.ActualHeight) : Location.Y + this.ActualHeight;
+                line.X2 = line.X1;
+                line.Y2 = Location.Y + ChildLineWidth;
+            }
+            return line;
+        }
+
+        public Line GetParentLine()
+        {
+            Line line = new Line();
+            line.StrokeThickness = Convert.ToDouble(ConfigurationManager.AppSettings.Get("ParentLineThickness"));
+            line.Stroke = (Brush)Application.Current.Resources["ParentLineBrush"];
+            Point bottomAnchor = GetBottomAnchor();
+            line.X1 = bottomAnchor.X;
+            line.Y1 = bottomAnchor.Y;
+            line.X2 = line.X1;
+            line.Y2 = line.Y1 + ParentLineWidth - (0.5 * this.ActualHeight);
+            return line;
+        }
+
+        public Line GetSpouseLine()
+        {
+            Line line = new Line();
+            line.StrokeThickness = Convert.ToDouble(ConfigurationManager.AppSettings.Get("SpouseLineThickness"));
+            line.Stroke = (Brush)Application.Current.Resources["SpouseLineBrush"];
+            // Male
+            if(Person.Gender == Gender.Male)
+            {
+                line.X1 = GetRightAnchor().X;
+                line.Y1 = GetRightAnchor().Y;
+                line.X2 = GetRightAnchor().X + SpouseLineWidth;
+                line.Y2 = line.Y1;
+            }
+            // Female
+            if (Person.Gender == Gender.Female)
+            {
+                line.X1 = GetLeftAnchor().X;
+                line.Y1 = GetLeftAnchor().Y;
+                line.X2 = GetLeftAnchor().X - SpouseLineWidth;
+                line.Y2 = line.Y1;
+            }
+            return line;
+        }
+
+        public Point GetSpouseLocation()
+        {
+            Point SpouseLocation = new Point();
+
+            if(Person.Gender == Gender.Male)
+            {
+                SpouseLocation.X = Location.X + this.ActualWidth + SpouseLineWidth;
+                SpouseLocation.Y = Location.Y;
+            }
+            else
+            {
+                SpouseLocation.X = Location.X - this.ActualWidth - SpouseLineWidth;
+                SpouseLocation.Y = Location.Y;
+            }
+            return SpouseLocation;
+        }
+
+        public Point GetChildLocation(bool hasSpouse)
+        {
+            Point ChildLocation = new Point();
+            
+            if(Person.Gender == Gender.Male)
+            {
+                if(hasSpouse)
+                {
+                    ChildLocation.X = (Location.X + this.ActualWidth + ChildLineXOffset) - (0.5 * this.ActualWidth);
+                    ChildLocation.Y = Location.Y + ChildLineWidth;
+                }
+                else
+                {
+                    ChildLocation.X = Location.X;
+                    ChildLocation.Y = Location.Y + ChildLineWidth;
+                }
+            } 
+            else
+            { 
+                ChildLocation.X = (Location.X - this.ActualWidth - ChildLineXOffset) + (0.5 * this.ActualWidth);
+                ChildLocation.Y = Location.Y + ChildLineWidth;
+            }
+            return ChildLocation;
+        }
+
+        public Point GetParentLocation(bool parentHasSpouse, TreeMember parent)
+        {
+            Point ParentLocation = new Point();
+            if (parentHasSpouse)
+            {
+                if(parent.Person.Gender == Gender.Male)
+                {
+                    ParentLocation.X = (Location.X + (0.5 * this.ActualWidth)) - (0.5 * parent.SpouseLineWidth) - parent.ActualWidth;
+                    ParentLocation.Y = Location.Y + ParentLineWidth;
+                }
+                else
+                {
+                    ParentLocation.X = (Location.X + (0.5 * this.ActualWidth)) + (0.5 * parent.SpouseLineWidth);
+                    ParentLocation.Y = Location.Y + ParentLineWidth;
+                }
+            }
+            else
+            {
+                ParentLocation.X = Location.X;
+                ParentLocation.Y = Location.Y + ParentLineWidth;
+            }
+
+            return ParentLocation;
+        }
+
+        private double GetGeneration(double lineWidth)
+        {
+            double maxWidth = Convert.ToDouble(ConfigurationManager.AppSettings.Get("Generation0LineWidth"));
+            double maxWidthDividedByCurrentWidth = maxWidth / lineWidth;
+            return Math.Log(maxWidthDividedByCurrentWidth, 2);
+        }
+
+        private double CalculateTreeMemberNameFontSize(double generation)
+        {
+            double initialFontSize = Convert.ToDouble(ConfigurationManager.AppSettings.Get("TreeMemberNameFontSize"));
+            if (generation == 0)
+            {
+                return initialFontSize;
+            }
+            return initialFontSize / generation;
+        }
+
+        private double CalculateTreeMemberDateFontSize(double generation)
+        {
+            double initialFontSize = Convert.ToDouble(ConfigurationManager.AppSettings.Get("TreeMemberDateFontSize"));
+            if(generation == 0)
+            {
+                return initialFontSize;
+            }
+            return initialFontSize / generation;
+        }
+    }
+}
